8315e9d96694ee132d4dd1a352ed80b099a25b65
Dook97 <30779172+Dook97@users.noreply.github.com>
2021-06-28 23:57:16 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let canvas,
+    context,
+    edge,
+    point,
+    piContainer,
+    piValues = [0, 0];
+
+window.addEventListener('load', () => {
+    edge = 750;
+    canvas = document.querySelector('canvas');
+    piContainer = document.querySelector('#pi_container')
+    canvas.setAttribute('width', edge);
+    canvas.setAttribute('height', edge);
+    context = canvas.getContext('2d');
+
+    setInterval(() => {
+        generatePoint();
+        calculatePI();
+        draw();
+    }, 0);
+});
+
+let generatePoint = () => {
+    let x = Math.random() * canvas.width;
+        y = Math.random() * canvas.width;
+    point = {
+        x : x,
+        y : y,
+        inOut: Math.sqrt((x - edge / 2)**2 + (y - edge / 2)**2) < edge / 2
+    };
+};
+
+let calculatePI = () => {
+    point.inOut ? piValues[0] += 1 : piValues[1] += 1;
+    piContainer.textContent = (4 * piValues[0] / (piValues[0] + piValues[1])).toLocaleString('en-US', {minimumFractionDigits: 16, useGrouping:false})
+};
+
+let draw = () => {
+    point.inOut ? context.fillStyle = 'green' : context.fillStyle = 'red';
+    context.fillRect(point.x, point.y, 1, 1);
+};
