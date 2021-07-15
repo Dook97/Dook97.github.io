d6f5e8c65702575b4af2a0bbbb86fbd1c9ae91b9
Dook97 <30779172+Dook97@users.noreply.github.com>
2021-07-15 13:23:39 +0000

    working implementation of game of life without initial setup

--- js/gameoflife/gol.js
+++ js/gameoflife/gol.js
@@ -1,41 +1,74 @@
 let canvas,
     context,
-    field,
-    dimensions = {
-        x : 50,
-        y : 50
-    },
-    interval = 100,
     edge = 750,
-    scale = 10;
+    scale = 10, // edge MUST be divisible by scale
+    limitXY = edge/scale,
+    grid,
+    interval = 100;
 
 window.addEventListener('load', () => {
     canvas = document.querySelector('canvas');
     canvas.setAttribute('width', edge);
     canvas.setAttribute('height', edge);
     context = canvas.getContext('2d');
-    field = fillField();
+    grid = fillGrid();
 
-    setInterval(() => field = updateField(), interval);
+    setInterval(() => grid = updateGrid(), interval);
     draw();
 });
 
-let draw = () => {
-    //code duh
+const draw = () => {
+    for (let x = 0; x < limitXY; x++) {
+        for (let y = 0; y < limitXY; y++) {
+            context.fillStyle = grid[x][y] ? 'green' : 'black';
+            context.fillRect(x*scale, y*scale, scale, scale);
+        }
+    }
     requestAnimationFrame(draw);
 };
 
-let updateField = () => {
-    //logic
-};
+const updateGrid = () => {
+    let out = fillGrid();
 
-let fillField = () => {
-    field = [];
-    for (let i = 0; i < dimensions.y; i++) {
-        field.push(new Array());
-        for (let j = 0; j < dimensions.x; j++) {
-            field[i].push(false);
+    for (let x = 0; x < limitXY; x++) {
+        for (let y = 0; y < limitXY; y++) {
+            let count = 0;
+            let neighbours = getNeighbours(x, y);
+            for (const nbr of neighbours) {
+                if (grid[nbr[0]][nbr[1]]) {
+                    count++;
+                }
+            }
+            out[x][y] = (grid[x][y] && count === 2) || (count === 3);
         }
     }
-    return field;
+    return out;
+};
+
+const getNeighbours = (x, y) => {
+    let neigbours = [
+        [x-1,y-1],[x,y-1],[x+1,y-1],
+        [x-1,y  ],        [x+1,y  ],
+        [x-1,y+1],[x,y+1],[x+1,y+1]
+    ];
+    let out = []
+
+    for (let i = 0; i < neigbours.length; i++) {
+        if (neigbours[i][0] >= 0 && neigbours[i][1] >= 0 && neigbours[i][0] < limitXY && neigbours[i][1] < limitXY) {
+            out.push(neigbours[i]);
+        }
+    }
+    return out;
+};
+
+const fillGrid = () => {
+    let out = [];
+
+    for (let i = 0; i < limitXY; i++) {
+        out.push(new Array());
+        for (let j = 0; j < limitXY; j++) {
+            out[i].push(false);
+        }
+    }
+    return out;
 };
